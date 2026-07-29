0802ccad0675482b0e22702416ee76f5ec94588b
Stormy <memeanimatic@gmail.com>
2026-07-28 19:53:07 -0600

    hola profe como le va

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,17 +1,23 @@
+import java.sql.SQLException;
+
 public class Alumno {
     private int id;
     private String matricula;
     private String nombre;
-    private String edad;
+    private int edad;
     private String sexo;
     private String correo;
 
-    public Alumno(int id, String matricula, String nombre, String edad, String sexo, String correo) {
+    public Alumno(int id, String matricula, String nombre, int edad, String sexo, String correo) {
         this.id = id;
         this.matricula = matricula;
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
         this.correo = correo;
+    }
+
+    public void save() throws SQLException{
+
     }
 }
